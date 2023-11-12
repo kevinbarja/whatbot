84a0b1ddb68fcdd1a99e0b5fa7213f077c0a8464
Eduardo Barja <kevinbarja@gmail.com>
2023-11-12 11:00:08 -0400

    Send massive message

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on('authenticated', (session) => {
 
 // Ruta para enviar mensajes
 app.post('/send', async (req, res) => {
-    /*
+    
     const chats = await client.getChats();
     const chatObjects = chats.filter((chat) => chat.isGroup);
     const labels = chatObjects[0].getLabels();
@@ -35,7 +35,10 @@ app.post('/send', async (req, res) => {
         const groupChatObj = {'groupName': chatObject.groupMetadata.subject, 'id': chatObject.groupMetadata.id._serialized};
         return groupChatObj;
       });    
-    */
+    res.send(JSON.stringify(groupChats));
+    console.log(JSON.stringify(groupChats));
+    return;
+
     const chat = await client.getChatById('')
     ///console.log(`Chat id @${JSON.stringify(chat)}`);
     let text = "¿Qué dice la masa? Junte?\n\n\n";
@@ -54,6 +57,37 @@ app.post('/send', async (req, res) => {
     
     res.send('ok');
 });
+
+app.post('/send-video', async (req, res) => {
+  console.log(`send video`);
+  const chat = await client.getChatById('59175001599-1527638324@g.us')
+  ///console.log(`Chat id @${JSON.stringify(chat)}`);
+  let text = "*Te estamos buscando campeón!*\n🕣¿Eres responsable y puntual?\n🤓¿Te animás a utilizar el sistema plan 150?\nEscríbenos al 75632256 (Edu Barja) y formá parte de nuestra comisión de Adjudicación de Entradas 🎫\n\n\n";
+  let mentions = [];
+
+  for(let participant of chat.participants) {
+      //const contact = await client.getContactById(participant.id._serialized);
+      if (participant.id._serialized != '59175001604@c.us' && participant.id._serialized != '59178073154@c.us'
+      && participant.id._serialized != '59178518045@c.us' && participant.id._serialized != '59175637765@c.us' && participant.id._serialized != '59177075240@c.us') {
+       mentions.push(participant.id._serialized);
+       text += `@${participant.id.user} `;
+      }
+  }
+
+  /*
+  const mediaMessage = new MessageMedia('video/mp4', req.body.base64, message);
+  client.sendMessage(from_number, mediaMessage, {caption : message, sendMediaAsDocument: true}).then(() => {
+    res.send('Mensaje enviado');
+  }).catch((error) => {
+    res.status(500).send(`Error al enviar el mensaje: ${error}`);
+  });
+  */
+
+  await chat.sendMessage(text, { mentions });    
+  
+  res.send('ok');
+});
+
 
 client.on('message', async (msg) => {
     if(msg.body === '!edu') {
